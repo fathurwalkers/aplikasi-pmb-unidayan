76f39f2bf8be199b6ab12f48584d802b723c09b6
Fathur Walkers <fathurwalkers44@gmail.com>
2022-03-01 00:02:58 -0800

    update

---
+++ resources/views/components/home-counts.blade.php
@@ -0,0 +1,43 @@
+<div>
+    <section id="counts" class="counts">
+        <div class="container">
+
+          <div class="row">
+
+            <div class="col-lg-4 col-md-4">
+              <div class="count-box">
+                <i class="fas fa-user-md"></i>
+                <span data-purecounter-start="0" data-purecounter-end="85" data-purecounter-duration="1" class="purecounter"></span>
+                <p>Pendaftaran Berhasil</p>
+              </div>
+            </div>
+
+            <div class="col-lg-4 col-md-4 mt-5 mt-md-0">
+              <div class="count-box">
+                <i class="far fa-hospital"></i>
+                <span data-purecounter-start="0" data-purecounter-end="18" data-purecounter-duration="1" class="purecounter"></span>
+                <p>Pendaftaran di Proses</p>
+              </div>
+            </div>
+
+            <div class="col-lg-4 col-md-4 mt-5 mt-lg-0">
+              <div class="count-box">
+                <i class="fas fa-flask"></i>
+                <span data-purecounter-start="0" data-purecounter-end="12" data-purecounter-duration="1" class="purecounter"></span>
+                <p>Total Terdaftar</p>
+              </div>
+            </div>
+
+            {{-- <div class="col-lg-3 col-md-6 mt-5 mt-lg-0">
+              <div class="count-box">
+                <i class="fas fa-award"></i>
+                <span data-purecounter-start="0" data-purecounter-end="150" data-purecounter-duration="1" class="purecounter"></span>
+                <p>Awards</p>
+              </div>
+            </div> --}}
+
+          </div>
+
+        </div>
+      </section>
+</div>
---
+++ resources/views/components/home-navbar.blade.php
@@ -0,0 +1,10 @@
+<div>
+    <nav id="navbar" class="navbar order-last order-lg-0">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#hero">Beranda</a></li>
+          <li><a class="nav-link scrollto" href="#about">Informasi</a></li>
+          <li><a class="nav-link scrollto" href="#services">Pendaftaran</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav>
+</div>
