0ce7ec7e91d4461f51c1de21ba596a1ff86aca9b
Fathur Walkers <fathurwalkers44@gmail.com>
2022-03-01 02:18:58 -0800

    update

--- resources/views/components/home-gallery.blade.php
+++ resources/views/components/home-gallery.blade.php
@@ -10,69 +10,15 @@
       <div class="container-fluid">
         <div class="row g-0">
 
-          <div class="col-lg-3 col-md-4">
-            <div class="gallery-item">
-              <a href="{{ asset('medilab/assets') }}/img/gallery/gallery-1.jpg" class="galelry-lightbox">
-                <img src="{{ asset('medilab/assets') }}/img/gallery/gallery-1.jpg" alt="" class="img-fluid">
-              </a>
-            </div>
-          </div>
-
-          <div class="col-lg-3 col-md-4">
-            <div class="gallery-item">
-              <a href="{{ asset('medilab/assets') }}/img/gallery/gallery-2.jpg" class="galelry-lightbox">
-                <img src="{{ asset('medilab/assets') }}/img/gallery/gallery-2.jpg" alt="" class="img-fluid">
-              </a>
-            </div>
-          </div>
-
-          <div class="col-lg-3 col-md-4">
-            <div class="gallery-item">
-              <a href="{{ asset('medilab/assets') }}/img/gallery/gallery-3.jpg" class="galelry-lightbox">
-                <img src="{{ asset('medilab/assets') }}/img/gallery/gallery-3.jpg" alt="" class="img-fluid">
-              </a>
-            </div>
-          </div>
-
-          <div class="col-lg-3 col-md-4">
-            <div class="gallery-item">
-              <a href="{{ asset('medilab/assets') }}/img/gallery/gallery-4.jpg" class="galelry-lightbox">
-                <img src="{{ asset('medilab/assets') }}/img/gallery/gallery-4.jpg" alt="" class="img-fluid">
-              </a>
-            </div>
-          </div>
-
-          <div class="col-lg-3 col-md-4">
-            <div class="gallery-item">
-              <a href="{{ asset('medilab/assets') }}/img/gallery/gallery-5.jpg" class="galelry-lightbox">
-                <img src="{{ asset('medilab/assets') }}/img/gallery/gallery-5.jpg" alt="" class="img-fluid">
-              </a>
-            </div>
-          </div>
-
-          <div class="col-lg-3 col-md-4">
-            <div class="gallery-item">
-              <a href="{{ asset('medilab/assets') }}/img/gallery/gallery-6.jpg" class="galelry-lightbox">
-                <img src="{{ asset('medilab/assets') }}/img/gallery/gallery-6.jpg" alt="" class="img-fluid">
-              </a>
-            </div>
-          </div>
-
-          <div class="col-lg-3 col-md-4">
-            <div class="gallery-item">
-              <a href="{{ asset('medilab/assets') }}/img/gallery/gallery-7.jpg" class="galelry-lightbox">
-                <img src="{{ asset('medilab/assets') }}/img/gallery/gallery-7.jpg" alt="" class="img-fluid">
-              </a>
-            </div>
-          </div>
-
-          <div class="col-lg-3 col-md-4">
-            <div class="gallery-item">
-              <a href="{{ asset('medilab/assets') }}/img/gallery/gallery-8.jpg" class="galelry-lightbox">
-                <img src="{{ asset('medilab/assets') }}/img/gallery/gallery-8.jpg" alt="" class="img-fluid">
-              </a>
-            </div>
-          </div>
+            @for ($i = 0; $i < 10; $i++)
+                <div class="col-lg-3 col-md-4">
+                    <div class="gallery-item">
+                    <a href="{{ asset('default-img') }}/default{{$i}}.jpg" class="galelry-lightbox">
+                        <img src="{{ asset('default-img') }}/default{{$i}}.jpg" alt="" class="img-fluid">
+                    </a>
+                    </div>
+                </div>
+            @endfor
 
         </div>
 
