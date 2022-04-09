33e71af9de0fae63f4f399c6d48c6ee7dba2331e
Fathur Walkers <fathurwalkers44@gmail.com>
2022-04-09 06:53:05 -0700

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,91 +17,83 @@ class DatabaseSeeder extends Seeder
     {
         $faker = Faker::create('id_ID');
 
-                // =======================================================================================
-        // DATA PRODI
-
+        // =======================================================================================
         $prodi = new Prodi;
-
-        $array_prodi = [
-            [
-                "Drs. Ilham, M.Si.",
-                "Hartini Amin, S.Sos., M.Si.",
-                "Haeruddin, S.Pd., M.A.",
-                "La Sariade, S.Pd., M.Pd.",
-                "La Eru Ugi, S.Pd,M.Pd",
-                "Rizal Arisman, S.Pd., M.Pd.",
-                "Iip Irawan Hisanuddin, S.E., M.M.",
-                "Said Saleh Salihi, MSA,S.E.,Ak",
-                "Sumitro, S.Pi., M.Si.",
-                "Nasrin, S.H., M.H.",
-                "La Ode Asman Muriman, S.T., M.T.",
-                "Ahmad Gasruddin, S.T., M.T.",
-                "Ery Muchyar Hasiri, S.Kom., M.T.",
-                "Sarman, S.T., M.T.",
-                "La Ode Muh. Taufiq, SKM., M.Kes.",
-                "Badaria, S.Si., M.Sc.",
-                "Dr. La Didi, S.I.P., MAP",
-                "Dr. Juamdan Zamha Zamihu, S.S., M.Hum."
-            ],
-            [
-                "Ilmu Administrasi Negara",
-                "Sosiologi",
-                "Pendidikan Sejarah",
-                "Pendidikan Ekonomi",
-                "Pendidikan Matematika",
-                "Pendidikan Bahasa Inggris",
-                "Manajemen",
-                "Akuntansi",
-                "Budidaya Perairan",
-                "Ilmu Hukum",
-                "Teknik Mesin",
-                "Teknik Sipil",
-                "Teknik Informatika",
-                "Teknik Pertambangan",
-                "Kesehatan Masyarakat",
-                "Agroteknologi",
-                "Ilmu Administrasi Negara",
-                "Pendidikan Bahasa Inggris"
-            ],
-            [
-                "S1",
-                "S1",
-                "S1",
-                "S1",
-                "S1",
-                "S1",
-                "S1",
-                "S1",
-                "S1",
-                "S1",
-                "S1",
-                "S1",
-                "S1",
-                "S1",
-                "S1",
-                "S1",
-                "S2",
-                "S2"
-            ]
+        $array_prodi_pimpinan = [
+            "Drs. Ilham, M.Si.",
+            "Hartini Amin, S.Sos., M.Si.",
+            "Haeruddin, S.Pd., M.A.",
+            "La Sariade, S.Pd., M.Pd.",
+            "La Eru Ugi, S.Pd,M.Pd",
+            "Rizal Arisman, S.Pd., M.Pd.",
+            "Iip Irawan Hisanuddin, S.E., M.M.",
+            "Said Saleh Salihi, MSA,S.E.,Ak",
+            "Sumitro, S.Pi., M.Si.",
+            "Nasrin, S.H., M.H.",
+            "La Ode Asman Muriman, S.T., M.T.",
+            "Ahmad Gasruddin, S.T., M.T.",
+            "Ery Muchyar Hasiri, S.Kom., M.T.",
+            "Sarman, S.T., M.T.",
+            "La Ode Muh. Taufiq, SKM., M.Kes.",
+            "Badaria, S.Si., M.Sc.",
+            "Dr. La Didi, S.I.P., MAP",
+            "Dr. Juamdan Zamha Zamihu, S.S., M.Hum."
         ];
-
-        $count_prodi = count($array_prodi[0]);
-
-        foreach ($array_prodi as $item) {
+        $array_prodi_nama = [
+            "Ilmu Administrasi Negara",
+            "Sosiologi",
+            "Pendidikan Sejarah",
+            "Pendidikan Ekonomi",
+            "Pendidikan Matematika",
+            "Pendidikan Bahasa Inggris",
+            "Manajemen",
+            "Akuntansi",
+            "Budidaya Perairan",
+            "Ilmu Hukum",
+            "Teknik Mesin",
+            "Teknik Sipil",
+            "Teknik Informatika",
+            "Teknik Pertambangan",
+            "Kesehatan Masyarakat",
+            "Agroteknologi",
+            "Ilmu Administrasi Negara",
+            "Pendidikan Bahasa Inggris"
+        ];
+        $array_prodi_tingkatan = [
+            "S1",
+            "S1",
+            "S1",
+            "S1",
+            "S1",
+            "S1",
+            "S1",
+            "S1",
+            "S1",
+            "S1",
+            "S1",
+            "S1",
+            "S1",
+            "S1",
+            "S1",
+            "S1",
+            "S2",
+            "S2"
+        ];
+        $count_prodi = count($array_prodi_nama);
+        for ($i=0; $i < $count_prodi; $i++) {
             $prodi_kode = "PRODI" . Str::random(5);
             $save_prodi = $prodi->create([
                 "prodi_kode" => $prodi_kode,
-                "prodi_nama" => $item[1],
-                "prodi_tingkatan" => $item[2],
+                "prodi_nama" => $array_prodi_nama[$i],
+                "prodi_tingkatan" => $array_prodi_tingkatan[$i],
                 "prodi_foto_pimpinan" => "home-default-profile.jpg",
-                "prodi_nama_pimpinan" => $item[0],
+                "prodi_nama_pimpinan" => $array_prodi_pimpinan[$i],
                 "created_at" => now(),
                 "updated_at" => now()
             ]);
             $save_prodi->save();
         }
         // ===========================================================================================
-
 
         // ADMIN
         $token = Str::random(16);
