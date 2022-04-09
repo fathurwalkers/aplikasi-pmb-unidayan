262e5c4d12087d07f6f7f018c63aac7d199dcc9c
Fathur Walkers <fathurwalkers44@gmail.com>
2022-04-08 23:39:26 -0700

    update

--- resources/views/home/pendaftaran.blade.php
+++ resources/views/home/pendaftaran.blade.php
@@ -8,7 +8,7 @@
         <div class="container">
         <h1>Website Pendaftaraan Mahasiswa Baru</h1>
         <h2>Universitas Dayanu Ikhsanuddin</h2>
-        <a href="#about" class="btn-get-started scrollto">Mulai Pendaftaran</a>
+        {{-- <a href="#about" class="btn-get-started scrollto">Mulai Pendaftaran</a> --}}
         </div>
     </section>
     <!-- End Hero -->
