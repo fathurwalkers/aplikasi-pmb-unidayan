3b86537dd360679ae1ad57683e1cc9755418be7e
Fathur Walkers <fathurwalkers44@gmail.com>
2022-05-03 09:59:05 -0700

    update

---
+++ resources/views/home/informasi.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.home-layout')
+
+@section('title', 'Halaman Informasi')
+
+@section('header-content')
+    <!-- ======= Hero Section ======= -->
+    <section id="hero" class="d-flex align-items-center">
+        <div class="container">
+        <h1>Website Pendaftaraan Mahasiswa Baru</h1>
+        <h2>Universitas Dayanu Ikhsanuddin</h2>
+        {{-- <a href="#about" class="btn-get-started scrollto">Mulai Pendaftaran</a> --}}
+        </div>
+    </section>
+    <!-- End Hero -->
+@endsection
+
+@push('css')
+<style>
+
+</style>
+@endpush
+
+@section('main-content')
+    <section id="counts" class="counts">
+
+        <div class="container">
+        </div>
+
+    </section>
+@endsection
+
+@push('js')
+<script>
+
+</script>
+@endpush
