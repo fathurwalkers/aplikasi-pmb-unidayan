46966d1c685eae5994e1c24c5b7c0b07fd66c176
Fathur Walkers <fathurwalkers44@gmail.com>
2022-05-18 11:24:25 -0700

    update

--- resources/views/home/lihat-informasi.blade.php
+++ resources/views/home/lihat-informasi.blade.php
@@ -24,7 +24,28 @@
     <section id="counts" class="counts">
 
         <div class="container">
-
+            <div class="card">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-sm-12 col-md-12 col-lg-12 d-flex justify-content-center">
+                            <h5>
+                                <b>{{ $informasi->informasi_title }}</b>
+                            </h5>
+                        </div>
+                    </div>
+                    <hr />
+                    <div class="row mb-2">
+                        <div class="col-sm-12 col-md-12 col-lg-12">
+                            Diterbitkan pada : {{ date("d/M/Y", strtotime($informasi->informasi_waktu)) }}
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-12 col-md-12 col-lg-12">
+                            {!! $informasi->informasi_body !!}
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
 
     </section>
